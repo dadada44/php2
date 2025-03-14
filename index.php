7daeaf1cae08abec2f1519f6dbd7286d4584e5f2
Daryna Toporovska <Daryna.Toporovska@student.souepl.cz>
2025-03-14 11:27:45 +0100

    databaze nove

--- index.php
+++ index.php
@@ -13,35 +13,63 @@
 </html>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "prvni_db";
+      $servername = "localhost";
+      $username = "root";
+      $password = "root";
+      $dbname = "prvni_db";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+      // Create connection
+      $conn = mysqli_connect($servername, $username, $password, $dbname);
+      // Check connection
+      if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
 
-$sql = "SELECT 
-ID,
-Nazev_produktu,
-Cena_za_kus,
-Pocet_kusu FROM zbozi";
-$result = mysqli_query($conn, $sql);
+      $sql = "SELECT 
+      ID,
+      Nazev_produktu,
+      Cena_za_kus,
+      Pocet_kusu FROM zbozi";
+      $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["ID"]. " - Name: " . $row["Nazev_produktu"]. " " . $row["Cena_za_kus"]. " KČ".  "<br>";
-  }
-} else {
-  echo "0 results";
-}
+      
+    if ($result->num_rows > 0) {
+        echo "<table>
+                <tbody>
+                    <tr>
+                        <th>Id</th>
+                        <th>Název produktu</th>
+                        <th>Cena za kus</th>
+                        <th>Počet kusů</th>
+                    </tr>";
+    
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>" . $row["ID"] . "</td> 
+                    <td>" . $row["Nazev_produktu"] . "</td>
+                    <td>" . $row["Cena_za_kus"] . " Kč</td>
+                    <td>" . $row["Pocet_kusu"] . "</td>
+                  </tr>";
+        }
 
-mysqli_close($conn);
+        echo "</tbody>
+        </table>";
+    } else {
+        echo "0 results";
+      }
+
+
 ?>
 
+
+<style>
+  table{
+    border: 1px solid black;
+  }
+
+  th, td{
+    border: 1px solid black;
+
+  }
+</style>
 
